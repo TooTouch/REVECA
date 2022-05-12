448fcf12faa3f4649c372dd53fa9c1507f861a09
tootouch <wogur379@gmail.com>
2022-05-12 21:03:05 +0900

    [shell] update run script

--- GEBC/run.sh
+++ GEBC/run.sh
@@ -1,1 +1,6 @@
-python run_captioning.py --do_train --evaluate_during_training --gpu_ids "0 1 2 3" --per_gpu_train_batch_size 4 --wandb
+python run_captioning.py --do_train --evaluate_during_training \
+                         --gpu_ids "0 1 2 3" \
+                         --per_gpu_train_batch_size 4 \
+                         --per_gpu_eval_batch_size 8 \
+                         --save_steps 10000 \
+                         --wandb
